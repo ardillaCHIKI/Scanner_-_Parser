2a70ff8fd3e4f35fc9ebe15fead91fc96f148b03
ardillaCHIKI <svegazos@student.universidadunie.com>
2025-12-30 10:10:20 +0100

    ++

--- Scanner/MiniLexer.java
+++ Scanner/MiniLexer.java
@@ -1,4 +1,6 @@
 package Scanner;
+
+// import java.util.Scanner;
 public class MiniLexer {
     public static TipoToken clasificarToken(String lexema) {
 
@@ -23,4 +25,25 @@ public class MiniLexer {
         }
     }
 	
+    // public static void main(String[] args) {
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Introduce instrucciones simples:");
+        // String entrada = sc.nextLine();
+
+        // String[] tokensEntrada = entrada.split("\\s+");
+
+        // Token[] listaTokens = new Token[tokensEntrada.length];
+        
+        // for (int i = 0; i < tokensEntrada.length; i++) {
+            // TipoToken tipo = clasificarToken(tokensEntrada[i]);
+            // listaTokens[i] = new Token(tipo, tokensEntrada[i]);
+        // }
+        
+        // System.out.println("Tokens encontrados:");
+        // for (Token token : listaTokens) {
+            // System.out.println(token);
+        // }
+        
+        // sc.close();
+	//}
 }
